66fdd76af3f96330ec21ed4b8787df9f5794b1c7
54Laichu <machi123j@gmail.com>
2024-06-20 14:07:31 +0800

    feat: user login/out requests from frontend

--- static/js/signinContainer.js
+++ static/js/signinContainer.js
@@ -1,3 +1,4 @@
+//signinContainer.js
 document.addEventListener('DOMContentLoaded', function() {
   const signInButton = document.querySelector('#sign-in-button');
   const signInContainer = document.querySelector('.sign-in-container');
@@ -5,7 +6,11 @@ document.addEventListener('DOMContentLoaded', function() {
   const closeBtn = document.querySelector('.close-btn');
   const registerBtn = document.querySelector('#register-btn')
   const loginForm = document.querySelector('#login-form');
+  const submitBtn = loginForm.querySelector("input[type='button']")
+  const inputField = loginForm.querySelector("input[type='text']");
+  const passwordField = loginForm.querySelector("input[type='password']");
 
+  // view effects
   signInButton.addEventListener('click', function() {
     signInContainer.style.display = 'block';
     overlay.style.display = 'block';
@@ -14,21 +19,25 @@ document.addEventListener('DOMContentLoaded', function() {
   overlay.addEventListener('click', function() {
     signInContainer.style.display = 'none';
     overlay.style.display = 'none';
+    inputField.value = '';
+    passwordField.value = '';
   });
 
   closeBtn.addEventListener('click', function() {
     signInContainer.style.display = 'none';
     overlay.style.display = 'none';
+    inputField.value = '';
+    passwordField.value = '';
   });
 
   registerBtn.addEventListener('click', function(event) {
     event.preventDefault();
     const nameDiv = document.createElement('div');
-    const submitBtn = loginForm.querySelector('input[type="submit"]')
 
     if (submitBtn.value === "登入帳戶") {
       const nameInput = document.createElement('input');
       nameInput.type = 'text';
+      nameInput.name = 'name';
       nameInput.placeholder = '輸入姓名';
       nameDiv.appendChild(nameInput);
       loginForm.insertBefore(nameDiv, loginForm.firstChild);
@@ -37,19 +46,13 @@ document.addEventListener('DOMContentLoaded', function() {
       registerBtn.textContent = '已經有帳戶了？點此登入';
       submitBtn.value = '註冊新帳戶';
 
-      loginForm.method = 'post';
-      loginForm.action = '/api/user';
     } else if ((submitBtn.value === "註冊新帳戶")) {
       loginForm.removeChild(loginForm.firstChild);
 
       signInContainer.querySelector('p').textContent = '登入會員帳號';
       registerBtn.textContent = '還沒有帳戶？點此註冊';
       submitBtn.value = '登入帳戶';
-
-      loginForm.method = 'put';
-      loginForm.action = '/api/user/auth';
     }
-
-
   })
+
 });
