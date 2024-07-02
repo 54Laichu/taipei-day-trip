48c72baf9c45908cabe67822282be8c317069363
54Laichu <machi123j@gmail.com>
2024-07-02 16:49:57 +0800

    feat: frontend tappay

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
-from controllers import attractions_controller, mrts_controller, users_controller, user_auth_controller, booking_controller
+from fastapi.responses import FileResponse, JSONResponse
+import os
+from controllers import attractions_controller, mrts_controller, users_controller, user_auth_controller, booking_controller, order_controller
 
 app = FastAPI()
 
@@ -21,8 +22,13 @@ async def booking(request: Request):
 async def thankyou(request: Request):
     return FileResponse("./static/thankyou.html", media_type="text/html")
 
+@app.get("/api/config")
+async def get_tappay_config():
+    return JSONResponse({"appId": os.environ["APP_ID"], "appKey": os.enviorn["APP_KEY"],})
+
 app.include_router(attractions_controller.router, prefix="/api", tags=["attraction"])
 app.include_router(mrts_controller.router, prefix="/api", tags=["mrt"])
 app.include_router(users_controller.router, prefix="/api", tags=["user"])
 app.include_router(user_auth_controller.router, prefix="/api", tags=["user_auth"])
 app.include_router(booking_controller.router, prefix="/api", tags=["booking"])
+app.include_router(order_controller.router, prefix="/api", tags=["order"])
